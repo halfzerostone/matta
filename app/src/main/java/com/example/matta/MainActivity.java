593e6bc401f07d3edcd91ce75c643002de51f3e5
pys <dustin05322@gmail.com>
2023-06-18 20:53:12 +0900

    폰트 변경 아이콘 추가 등등

--- app/src/main/java/com/example/matta/MainActivity.java
+++ app/src/main/java/com/example/matta/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.matta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.icu.util.Calendar;
 import android.icu.util.Output;
 import android.os.Bundle;
@@ -10,7 +12,9 @@ import android.os.Looper;
 import android.os.Message;
 import android.provider.DocumentsContract;
 import android.widget.GridLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,33 +28,245 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    TextView timerTextView;
-    private String URL ="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=대덕고";
-    String msg;
-    String datestring;
-    String lunchcheck;
-    String dinnercheck;
-    String regex="[^0-9]";
+    TextView textView;  //여기에 급식 식단표 표시
+    TextView timerTextView; //시간과 다음시간 표시
+    private String URL ="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=대덕고";  //급식 크롤링 대상 링크
+    String msg; //크롤링에 사용하는 내부 메시지
+    String datestring; //오늘 날짜를 자료화함(***같은 형식으로)
+    String lunchcheck;  //점심 메뉴 존재 체크용 변수
+    String dinnercheck; //저녁 메뉴 존재 체크용 변수
+    String regex="[^0-9]"; //정수 전부 제거하기 위한 0-9까지의 숫자
     final Bundle bundle=new Bundle();
-    String today=" TODAY";
-    Calendar calendar = Calendar.getInstance();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
-    String dat = dateFormat.format(calendar.getTime());
-    String name;
-    String cellnum;
+    String today=" TODAY";  //대체할 문자열(공백)
+    Calendar calendar = Calendar.getInstance(); //오늘 날짜 받는 변수
+    SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE", Locale.getDefault());    //날짜 받는 변수의 형태 선언
+    String dat = dateFormat.format(calendar.getTime()); //분 단위 변수 불러오기
+    String name;    //이번시간 이름을 시간표에서 받아오기
+    String cellnum; //
+    String arraynum;
     TextView cellTextView;
     String dateUI;
     String outputdate;
     String outputtime;
+    String CurrentSubject;
+    int Class=4;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    int myInt;
+    String Subject[][]=new String[10][10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=(TextView) findViewById(R.id.textViewMain);
         //textView.setTextSize(30);
         timerTextView=findViewById(R.id.textView2);
         timerTextView.setTextSize(30);
+
+        NumberPicker npker = findViewById(R.id.Classpick);
+
+        npker.setOnLongPressUpdateInterval(100);
+        npker.setWrapSelectorWheel(true);
+        npker.setMaxValue(11); //최대값
+        npker.setMinValue(1); //최소값
+
+        pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        editor = pref.edit();
+        myInt=pref.getInt("Class", 3);
+        npker.setValue(myInt);
+
+        Class=npker.getValue();
+
+        if(Class==4){
+            myInt=4;
+            editor.putInt("Class", myInt);
+            editor.apply();
+            Subject[1][1]="선택 A";
+            Subject[1][2]="국어";
+            Subject[1][3]="제2외국어";
+            Subject[1][4]="영어 A";
+            Subject[1][5]="영어 B";
+            Subject[1][6]="선택 C";
+            Subject[1][7]="창체";
+            Subject[2][1]="미감";
+            Subject[2][2]="선택 C";
+            Subject[2][3]="기하";
+            Subject[2][4]="선택 B";
+            Subject[2][5]="국어";
+            Subject[2][6]="영어 A";
+            Subject[2][7]="선택 A";
+            Subject[3][1]="기하";
+            Subject[3][2]="제2외국어";
+            Subject[3][3]="선택 A";
+            Subject[3][4]="국어";
+            Subject[3][5]="스생";
+            Subject[3][6]="선택 D";
+            Subject[3][7]="영어 B";
+            Subject[4][1]="기하";
+            Subject[4][2]="선택 C";
+            Subject[4][3]="스생";
+            Subject[4][4]="국어";
+            Subject[4][5]="선택 B";
+            Subject[4][6]="심리";
+            Subject[4][7]="선택 E";
+            Subject[5][1]="국어";
+            Subject[5][2]="영어 B";
+            Subject[5][3]="선택 B";
+            Subject[5][4]="진로";
+            Subject[5][5]="기하";
+            Subject[5][6]="자율";
+            Subject[5][7]="자율";
+            for(int i=1;i<5;i++){ //i요일
+                for(int j=1;j<7;j++){  //교시
+                    CurrentSubject=Subject[i][j];
+                    if(i==1&&j!=5){
+                        cellnum="cell_Monday"+j;
+                        int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                        cellTextView = findViewById(resId);
+                        System.out.println(CurrentSubject);
+                        cellTextView.setText(CurrentSubject);
+                    }else if(i==2&&j!=5){
+                        cellnum="cell_Tuesday"+j;
+                        int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                        cellTextView = findViewById(resId);
+                        cellTextView.setText(CurrentSubject);
+                    }else if(i==3&&j!=5){
+                        cellnum="cell_Wednesday"+j;
+                        int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                        cellTextView = findViewById(resId);
+                        cellTextView.setText(CurrentSubject);
+                    }else if(i==4&&j!=5){
+                        cellnum="cell_Thursday"+j;
+                        int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                        cellTextView = findViewById(resId);
+                        cellTextView.setText(CurrentSubject);
+                    }else if(i==5&&j!=5){
+                        cellnum="cell_Friday"+j;
+                        int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                        cellTextView = findViewById(resId);
+                        cellTextView.setText(CurrentSubject);
+                    }
+                }
+            }
+        }
+
+
+        npker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener(){
+
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+            {
+                Class=newVal;
+                if(Class==4){
+                    myInt=4;
+                    editor.putInt("Class", myInt);
+                    editor.apply();
+                    Subject[1][1]="선택 A";
+                    Subject[1][2]="국어";
+                    Subject[1][3]="제2외국어";
+                    Subject[1][4]="영어 A";
+                    Subject[1][5]="영어 B";
+                    Subject[1][6]="선택 C";
+                    Subject[1][7]="창체";
+                    Subject[2][1]="미감";
+                    Subject[2][2]="선택 C";
+                    Subject[2][3]="기하";
+                    Subject[2][4]="선택 B";
+                    Subject[2][5]="국어";
+                    Subject[2][6]="영어 A";
+                    Subject[2][7]="선택 A";
+                    Subject[3][1]="기하";
+                    Subject[3][2]="제2외국어";
+                    Subject[3][3]="선택 A";
+                    Subject[3][4]="국어";
+                    Subject[3][5]="스생";
+                    Subject[3][6]="선택 D";
+                    Subject[3][7]="영어 B";
+                    Subject[4][1]="기하";
+                    Subject[4][2]="선택 C";
+                    Subject[4][3]="스생";
+                    Subject[4][4]="국어";
+                    Subject[4][5]="선택 B";
+                    Subject[4][6]="심리";
+                    Subject[4][7]="선택 E";
+                    Subject[5][1]="국어";
+                    Subject[5][2]="영어 B";
+                    Subject[5][3]="선택 B";
+                    Subject[5][4]="진로";
+                    Subject[5][5]="기하";
+                    Subject[5][6]="자율";
+                    Subject[5][7]="자율";
+                    for(int i=1;i<5;i++){ //i요일
+                        for(int j=1;j<7;j++){  //교시
+                            CurrentSubject=Subject[i][j];
+                            if(i==1&&j!=5){
+                                cellnum="cell_Monday"+j;
+                                int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                                cellTextView = findViewById(resId);
+                                System.out.println(CurrentSubject);
+                                cellTextView.setText(CurrentSubject);
+                            }else if(i==2&&j!=5){
+                                cellnum="cell_Tuesday"+j;
+                                int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                                cellTextView = findViewById(resId);
+                                cellTextView.setText(CurrentSubject);
+                            }else if(i==3&&j!=5){
+                                cellnum="cell_Wednesday"+j;
+                                int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                                cellTextView = findViewById(resId);
+                                cellTextView.setText(CurrentSubject);
+                            }else if(i==4&&j!=5){
+                                cellnum="cell_Thursday"+j;
+                                int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                                cellTextView = findViewById(resId);
+                                cellTextView.setText(CurrentSubject);
+                            }else if(i==5&&j!=5){
+                                cellnum="cell_Friday"+j;
+                                int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                                cellTextView = findViewById(resId);
+                                cellTextView.setText(CurrentSubject);
+                            }
+                        }
+                    }
+                }//여기서 else if를 함으로서 반 설정
+            }
+        });
+
+        for(int i=1;i<5;i++){ //i요일
+            for(int j=1;j<7;j++){  //교시
+                CurrentSubject=Subject[i][j];
+                if(i==1&&j!=5){
+                    cellnum="cell_Monday"+j;
+                    int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                    cellTextView = findViewById(resId);
+                    System.out.println(CurrentSubject);
+                    cellTextView.setText(CurrentSubject);
+                }else if(i==2&&j!=5){
+                    cellnum="cell_Tuesday"+j;
+                    int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                    cellTextView = findViewById(resId);
+                    cellTextView.setText(CurrentSubject);
+                }else if(i==3&&j!=5){
+                    cellnum="cell_Wednesday"+j;
+                    int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                    cellTextView = findViewById(resId);
+                    cellTextView.setText(CurrentSubject);
+                }else if(i==4&&j!=5){
+                    cellnum="cell_Thursday"+j;
+                    int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                    cellTextView = findViewById(resId);
+                    cellTextView.setText(CurrentSubject);
+                }else if(i==5&&j!=5){
+                    cellnum="cell_Friday"+j;
+                    int resId = getResources().getIdentifier(cellnum, "id", getPackageName());
+                    cellTextView = findViewById(resId);
+                    cellTextView.setText(CurrentSubject);
+                }
+            }
+        }
+
         for (int column = 0; column < 6; column++) {
             for (int row = 0; row < 9; row++) {
                 if(column==0&&row!=5){
@@ -140,7 +356,7 @@ public class MainActivity extends AppCompatActivity {
                     date=doc.select(".cm_date").get(1);
                     datestring=date.text();
                     datestring=datestring.replaceAll((today),"");
-                    Output+="---------------------------------------"+"\n";
+                    Output+="---------------------"+"\n";
                     dinnercheck=datestring.replaceAll(regex,"");
                     if(lunchcheck.equals(dinnercheck)){
 
